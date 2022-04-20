59640e40d7e96404031a41f0f32ef09d1394af2c
kan1657 <kan1657@hotmail.com>
2022-04-20 15:04:02 +0700

    Finish login logout signup functionality

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import Banner from '../components/Banner'
 
 import Header from '../components/Header'
 import Row from '../components/Row'
+import useAuth from '../hooks/useAuth'
 import { Movie } from '../typings'
 import requests from '../utils/requests'
 
@@ -27,6 +28,9 @@ const Home = ({
   topRated,
   trendingNow,
 }: Props) => {
+  const { logout, loading } = useAuth()
+
+  if (loading) return null
   return (
     <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
+import useAuth from '../hooks/useAuth'
 
 interface Inputs {
   email: string
@@ -10,6 +11,8 @@ interface Inputs {
 
 function login() {
   const [login, setLogin] = useState(false)
+  const { signIn, signUp } = useAuth()
+
   const {
     register,
     handleSubmit,
@@ -18,9 +21,9 @@ function login() {
   } = useForm<Inputs>()
   const onSubmit: SubmitHandler<Inputs> = async ({ email, password }) => {
     if (login) {
-      // await signIn(email, password)
+      await signIn(email, password)
     } else {
-      // await signUp(email, password)
+      await signUp(email, password)
     }
   }
 
